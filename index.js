83f13f1d86e52f6d592c4e3955fa449791f13037
m.sofiichuk <koyap@ukr.net>
2022-09-12 17:38:00 +0300

    completed oop-exercise

--- index.js
+++ index.js
@@ -1,30 +1,61 @@
-import { print } from './js/lib.js';
-/* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
-   Complete the below for code reviewers' convenience:
+class Animal {
+    constructor(species, name, gender, legs, arms, saying) {
+        this.species = species;
+        this.name = name;
+        this.gender = gender;
+        this.legs = legs;
+        this.arms = arms;
+        this.saying = saying;
+    }
+}
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
-   */
+class Human extends Animal {
+    constructor(species = 'human', name, gender, legs = 2, arms = 2, saying) {
+        super(species, name, gender, legs, arms, saying)
+    }
+}
 
-// ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+class Man extends Human {
+    constructor(name, gender = 'male', legs, arms, saying = 'hi') {
+        super('human', name, gender, legs, arms, saying)
+    }
+}
 
+class Woman extends Human {
+    constructor(name, gender = 'female', legs, arms, saying = 'hello') {
+        super('human', name, gender, legs, arms, saying)
+    }
+}
 
-// ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
+const Tom = new Man('Tom');
+const Dick = new Man('Dick');
+const Harry = new Man('Harry');
 
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
+const Polly = new Woman('Polly');
+const Molly = new Woman('Molly');
+const Mary = new Woman('Mary');
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
+class Dog extends Animal {
+    constructor(name, gender, legs = 4, arms, saying = 'woof') {
+        super('dog', name, gender, legs, arms, saying)
+        delete this.arms
+    }
+}
 
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+class Cat extends Animal {
+    constructor(name, gender, legs = 4, arms, saying = 'meow') {
+        super('cat', name, gender, legs, arms, saying)
+        delete this.arms
+    }
+}
+
+const Buddy = new Dog('Buddy', 'male');
+const Betty = new Dog('Betty', 'female');
+
+const Kitty = new Cat('Kitty', 'female');
+const Tiger = new Cat('Tiger', 'male');
+
+const creatures = [Tom, Dick, Harry, Polly, Molly, Mary, Buddy, Betty, Kitty, Tiger];
+const creaturePropsList = creatures.map(props => `${Object.values(props).join(";")}`)
+    .join('\n\n');
+print(creaturePropsList);
