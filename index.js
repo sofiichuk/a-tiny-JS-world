761817dfbb458fa74e3a07a9778462684519fc95
m.sofiichuk <koyap@ukr.net>
2022-09-27 19:59:29 +0300

    fixed printing issues

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ class Animal {
         this.gender = gender;
         this.saying = saying;
     }
+
+    declareAnimal() {
+        return [this.name, this.gender, this.saying].join(";");
+    }
 }
 
 class Human extends Animal {
@@ -23,25 +27,41 @@ class Pet extends Animal {
 
 class Man extends Human {
     constructor(name, gender = 'male', saying = 'hi', legs, arms) {
-        super(name, gender, saying, legs, arms);
+        super(name, gender, saying, legs, arms)
+    }
+
+    declareGenus() {
+        return [this.constructor.name, super.declareAnimal(), this.legs, this.arms].join(";");
     }
 }
 
 class Woman extends Human {
     constructor(name, gender = 'female', saying = 'hello', legs, arms) {
-        super(name, gender, saying, legs, arms);
+        super(name, gender, saying, legs, arms)
+    }
+
+    declareGenus() {
+        return [this.constructor.name, super.declareAnimal(), this.legs, this.arms].join(";");
     }
 }
 
 class Dog extends Pet {
     constructor(name, gender, saying = 'woof', legs) {
-        super(name, gender, saying, legs);
+        super(name, gender, saying, legs)
+    }
+
+    declareGenus() {
+        return [this.constructor.name, super.declareAnimal(), this.legs].join(";");
     }
 }
 
 class Cat extends Pet {
     constructor(name, gender, saying = 'meow', legs) {
-        super(name, gender, saying, legs);
+        super(name, gender, saying, legs)
+    }
+
+    declareGenus() {
+        return [this.constructor.name, super.declareAnimal(), this.legs].join(";");
     }
 }
 
@@ -60,6 +80,5 @@ const Kitty = new Cat('Kitty', 'female');
 const Tiger = new Cat('Tiger', 'male');
 
 const creatures = [Tom, Dick, Harry, Polly, Molly, Mary, Buddy, Betty, Kitty, Tiger];
-const creaturePropsList = creatures.map(props => `${Object.values(props).join(";")}`)
-    .join('\n\n');
+const creaturePropsList = creatures.map(one => one.declareGenus()).join('\n\n');
 print(creaturePropsList);
